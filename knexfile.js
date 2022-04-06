3fa08a1429405a2d0ea41425469976e2bda0407e
Your Name <mari@Maribels-MacBook-Pro.local>
2022-04-05 21:29:41 -0500

    added folders and files to the api also added migration

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,18 @@
 // Update with your config settings.
 
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    ...sharedConfig,
     connection: {
       filename: './dev.sqlite3'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
